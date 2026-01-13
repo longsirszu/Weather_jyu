cde60c3723d19afb9219ea21a8bd584435db5617
longsir_47 <1726329396@qq.com>
2026-01-13 16:38:50 +0800

    初始化

---
+++ src/test/java/cn/edu/jyu/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.jyu.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
